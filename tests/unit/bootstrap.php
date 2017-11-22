af47aecf7eaec91c7072ba36c9bcca4fc67ff50b
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-22 13:41:22 +0100

    Use several Phing targets of Pfw_XH

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -5,9 +5,9 @@ require_once '../pfw/vendor/autoload.php';
 require_once '../../cmsimple/functions.php';
 require_once '../../cmsimple/adminfuncs.php';
 
-require_once './tests/FunctionMock.php';
-require_once './tests/RunkitFunctionMock.php';
-require_once './tests/UopzFunctionMock.php';
-require_once './tests/TestCase.php';
+require_once './tests/unit/FunctionMock.php';
+require_once './tests/unit/RunkitFunctionMock.php';
+require_once './tests/unit/UopzFunctionMock.php';
+require_once './tests/unit/TestCase.php';
 
 require_once '../pfw/classes/required_classes.php';
