2b9f9819725a41708f81f9dc677a69db3ecb4f61
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-22 18:53:09 +0100

    Fix CS violations in config and language files

--- config/config.php
+++ config/config.php
@@ -3,5 +3,3 @@
 $plugin_cf['themeswitcher']['prefer_page_theme']="true";
 $plugin_cf['themeswitcher']['display_automatic']="never";
 $plugin_cf['themeswitcher']['allowed_themes']="*";
-
-?>
--- config/metaconfig.php
+++ config/metaconfig.php
@@ -2,5 +2,3 @@
 
 $plugin_mcf['themeswitcher']['prefer_page_theme']="bool";
 $plugin_mcf['themeswitcher']['display_automatic']="enum:never,frontend,always";
-
-?>
