89c7d728b2ff2832c38405a26d9ae544bfb905ae
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-17 19:55:38 +0100

    Fix regressions of 1c3c6b6

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
 $plugin_cf['themeswitcher']['prefer_page_theme']="true";
-$plugin_cf['themeswitcher']['display_automatic']="always";
+$plugin_cf['themeswitcher']['display_automatic']="never";
 $plugin_cf['themeswitcher']['allowed_themes']="*";
