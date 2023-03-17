b370b5eed6fb09759c7b4964d369c1b05d6d73c8
Christoph M. Becker <cmbecker69@gmx.de>
2023-03-17 23:24:04 +0100

    Revise View and templates
    * Use short_open_tags in view templates during development
    * Auto-escaping of view values
    * Prevent direct access to view templates
    * Add header comments to view templates
    * 2 spaces indent in view templates

--- views/form.php
+++ views/form.php
@@ -2,6 +2,8 @@
 
 use Themeswitcher\Infra\View;
 
+if (!defined("CMSIMPLE_XH_VERSION")) {header("HTTP/1.1 403 Forbidden"); exit;}
+
 /**
  * @var View $this
  * @var string $class 
@@ -9,20 +11,21 @@ use Themeswitcher\Infra\View;
  * @var list<array{name:string,selected:string}> $themes
  */
 ?>
+<!-- themeswitcher selector -->
 <form class="themeswitcher_select_form <?=$class?>" method="get">
-    <input type="hidden" name="selected" value="<?=$selected?>">
-    <label>
-        <span><?=$this->text('label_theme')?></span>
-        <select name="themeswitcher_select" onchange="this.form.submit()">
-<?php foreach ($themes as $theme):?>
-            <option value="<?=$theme['name']?>" <?=$theme['selected']?>><?=$theme['name']?></option>
-<?php endforeach?>
-        </select>
-    </label>
-    <button class="themeswitcher_button"><?=$this->text('label_activate')?></button>
+  <input type="hidden" name="selected" value="<?=$selected?>">
+  <label>
+    <span><?=$this->text('label_theme')?></span>
+    <select name="themeswitcher_select" onchange="this.form.submit()">
+<?foreach ($themes as $theme):?>
+      <option value="<?=$theme['name']?>" <?=$theme['selected']?>><?=$theme['name']?></option>
+<?endforeach?>
+    </select>
+  </label>
+  <button class="themeswitcher_button"><?=$this->text('label_activate')?></button>
 </form>
 <script>
-    Array.prototype.forEach.call(document.getElementsByClassName("themeswitcher_button"), function (button) {
-        button.style.display = "none";
-    });
+  Array.prototype.forEach.call(document.getElementsByClassName("themeswitcher_button"), function (button) {
+    button.style.display = "none";
+  });
 </script>
--- views/info.php
+++ views/info.php
@@ -2,16 +2,19 @@
 
 use Themeswitcher\Infra\View;
 
+if (!defined("CMSIMPLE_XH_VERSION")) {header("HTTP/1.1 403 Forbidden"); exit;}
+
 /**
  * @var View $this
  * @var string $version
  * @var list<array{class:string,key:string,arg:string,statekey:string}> $checks
  */
 ?>
+<!-- themeswitcher info -->
 <h1>Themeswitcher <?=$version?></h1>
 <div>
-    <h2><?=$this->text('syscheck_title')?></h2>
-<?php foreach ($checks as $check):?>
-    <p class="<?=$check['class']?>"><?=$this->text($check['key'], $check['arg'])?><?=$this->text($check['statekey'])?></p>
-<?php endforeach?>
+  <h2><?=$this->text('syscheck_title')?></h2>
+<?foreach ($checks as $check):?>
+  <p class="<?=$check['class']?>"><?=$this->text($check['key'], $check['arg'])?><?=$this->text($check['statekey'])?></p>
+<?endforeach?>
 </div>
